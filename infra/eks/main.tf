a4c56b0592cf008fc00f9ba98484d86a42c946ca
andrius <andrius.korsakas@createfuture.com>
2025-12-01 15:52:20 +0000

    Changed postgres to StatefulSet

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -39,3 +39,33 @@ module "eks" {
     }
   }
 }
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+resource "kubernetes_storage_class" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+  reclaim_policy      = "Delete"
+
+  parameters = {
+    type   = "gp3"
+    fsType = "ext4"
+  }
+  
+  depends_on = [module.eks]
+}
